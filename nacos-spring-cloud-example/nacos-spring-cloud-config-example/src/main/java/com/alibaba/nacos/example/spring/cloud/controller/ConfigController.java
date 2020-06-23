fb328d0582c9a54f6a08b513e9de441b45dff839
wangtianqi20 <wangtianqi20@jd.com>
2020-06-23 18:39:23 +0800

    删除无用的health

--- nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/alibaba/nacos/example/spring/cloud/controller/ConfigController.java
+++ nacos-spring-cloud-example/nacos-spring-cloud-config-example/src/main/java/com/alibaba/nacos/example/spring/cloud/controller/ConfigController.java
@@ -5,6 +5,9 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author wangtianqi20
+ */
 @RestController
 @RequestMapping("/config")
 @RefreshScope
